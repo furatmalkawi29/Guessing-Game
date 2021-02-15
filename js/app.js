cfc21f08a3795c0e032b94a5d320926e743a9ba3
furatmalkawi29 <malkawifurat29@gmail.com>
2021-02-15 16:21:47 +0200

    ok2

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+// Guessing Game:
+
+let photography = prompt ("Do I Enjoy Photography?", 'y' );
+photography 
+
+
+
+let siblings = prompt ("Do I Have Older Siblings?" , 'y' );
+photography
+
+
+let carDriving = prompt ("Can I Drive a Car?" , 'n' );
+photography
+
+
+let catPerson = prompt ("Am I a Cat Person?" , 'y'  );
+photography
+
+
+let sweetTooth = prompt ("Am I a Sweet-tooth?" , 'y'  );
+photography
+
+
+
+
+
+"Do I Enjoy Photography?"
+"Do I Have Older Siblings?"
+"Can I Drive a Car?"
+"Am I a Cat Person?"
+"Am I a Sweet-tooth?"
+
+
