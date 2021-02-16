746a1285db0abe297ffb6f697b874f44cf2a8b87
furatmalkawi29 <malkawifurat29@gmail.com>
2021-02-16 21:53:57 +0200

    ok3

--- js/app.js
+++ js/app.js
@@ -2,87 +2,160 @@
 
 let score = 0;
 
-//Welcoming
+
+//Welcoming -------------------------------------------------------------------
 
 let inputName = prompt ('Enter Your Name Please:', 'Furat');
 
 alert ( 'Welcome ' + inputName + '!' );
 
 
+
+
+
 // Guessing Game:
 //Q1
-let photography = prompt ('Do I Enjoy Photography?', 'y' );
-photography = photography.toLowerCase();
 
-if (photography==='y')
-{
-  //console.log('Correct Answer! Yes, I Enjoy Photography? ');
-  alert ('Correct Answer! Yes, I Enjoy Photography? ');
-  score = score + 1;
+  let photography = prompt ('Do I Enjoy Photography? (y/n) or (yes/no)' , 'y' ).toLowerCase(); //get input
 
-} else {
-  alert ('Wrong Answer! NO, I Enjoy Photography? ');
+    while (photography !== 'n' && photography !== 'no' && photography !=='yes' && photography !== 'y' ) 
+    { // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+      photography = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
+    }
+
+
+    if (photography==='y'|| photography==='yes') // if answer is yes or y ---> alert
+    {
+      //console.log('Correct Answer! Yes, I Enjoy Photography? ');
+      alert ('Correct Answer! Yes, I Enjoy Photography? ');
+      score = score + 1;
+    } 
+    
+    else if (photography==='n'|| photography==='no') // check if answer is no or n ----> alert
+    {
+      alert ('Wrong Answer! NO, I Enjoy Photography? ');
+    }
+    
+  
+
+
+
+//-----------------------------------------------------------------------------
+//Q2
+let siblings = prompt ('Do I Have Older Siblings? (y/n) or (yes/no)' , 'n' ).toLowerCase();;
+
+while (siblings !== 'n' && siblings !== 'no' && siblings !=='yes' && siblings !== 'y' ) 
+{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+  siblings = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
 }
 
-//----------------------------------------------
-//Q2
-let siblings = prompt ('Do I Have Older Siblings?' , 'n' );
-siblings = siblings.toLowerCase();
-
-if (siblings==='n')
+if (siblings==='n' || siblings==='no') //check if answer no / n  ----> alert
 {
   //console.log('Correct Answer! Yes, I Do Not Have Older Siblings ');
   alert ('Correct Answer! Yes, I Do Not Have Older Siblings ');
   score = score + 1;
-} else {
+} 
+
+
+
+else if (siblings==='y'|| siblings==='yes') // check if answer is yes or y  ----> alert
+{
   alert ('Wrong Answer! NO, I Do Not Have Older Siblings ');
 }
 
-//----------------------------------------------
+
+
+
+
+//-------------------------------------------------------------------------
 //Q3
-let carDriving = prompt ('Can I Drive a Car?' , 'n' );
+let carDriving = prompt ('Can I Drive a Car? (y/n) or (yes/no)' , 'n' );
 carDriving = carDriving.toLowerCase();
 
-if (carDriving==='n')
+
+while (carDriving !== 'n' && carDriving !== 'no' && carDriving !=='yes' && carDriving !== 'y' ) 
+{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+  carDriving = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
+}
+
+
+if (carDriving==='n'|| carDriving==='no') //check if answer no / n
 {
   //console.log('Correct Answer! Yes, I Can not Drive a Car Yet ');
   alert ('Correct Answer! Yes, I Can not Drive a Car Yet ');
   score = score + 1;
-} else {
-  alert ('Wrong Answer! NO, I Can not Drive a Car Yet ');
+} 
+
+
+
+else if (carDriving==='y'|| carDriving==='yes') // check if answer is yes or y 
+{
+alert ('Wrong Answer! NO, I Can not Drive a Car Yet ');
 }
 
 
-//----------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
 //Q4
-let catPerson = prompt ('Am I a Cat Person?' , 'y' );
+let catPerson = prompt ('Am I a Cat Person? (y/n) or (yes/no)' , 'y' );
 catPerson = catPerson.toLowerCase();
 
+while (catPerson !== 'n' && catPerson !== 'no' && catPerson !=='yes' && catPerson !== 'y' ) 
+{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+  catPerson = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
+}
 
-if (catPerson==='y')
+
+if (catPerson==='y'|| catPerson==='yes') // check if answer is Yes or y 
 {
   //console.log('Correct Answer! Yes, I Am a Cat Person ');
   alert ('Correct Answer! Yes, I Am a Cat Person ');
   score = score + 1;
-} else {
+} 
+
+
+else if (catPerson==='n'|| catPerson==='no') // check if answer is no or n
+{
   alert ('Wrong Answer! NO, I Am a Cat Person ');
 }
 
 
 
-//----------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
 //Q5
-let sweetTooth = prompt ('Am I a Sweet-tooth?' , 'y' );
+let sweetTooth = prompt ('Am I a Sweet-tooth? (y/n) or (yes/no)' , 'y' );
 sweetTooth = sweetTooth.toLowerCase();
 
-if (sweetTooth==='y')
+while (sweetTooth !== 'n' && sweetTooth !== 'no' && sweetTooth !=='yes' && sweetTooth !== 'y' ) 
+{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+  sweetTooth = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
+}
+
+if (sweetTooth==='y'|| sweetTooth==='yes') // check if answer is Yes or y 
 {
   //console.log('Correct Answer! Yes, I Am a Sweet-tooth ');
   alert ('Correct Answer! Yes, I Am a Sweet-tooth ');
   score = score + 1;
-} else {
+} 
+
+
+else if (sweetTooth==='n'|| sweetTooth==='no') // check if answer is no or n
+{
   alert ('Wrong Answer! NO, I Am a Sweet-tooth ');
 }
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------
 
 //Q6 (Number)
 let numberInput = prompt ('Lets PLAY! | Guess What Number I Have In My Head ! ' , '5')
@@ -108,6 +181,10 @@ for (let i=0 ; i< 3 ;i++) // 4 attempts
 }
 
 
+
+
+
+//------------------------------------------------------------------------------------
 //Q7
 
 let arrChocolate =['galaxy','snickers','twix'];
@@ -141,6 +218,9 @@ alert ('You Didnt Guess Right! MY Favorites are : Galaxy, Mars, Twix '); //Out o
  
 }
 
+
+
+//SCORE -----------------------------------------------------------------------------------------
 
 alert ('YOUR SCORE IS  :' + score + '/ 7' ); //score
 
