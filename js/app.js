316c38c8aae80da729b596ff14cd55923d10f006
furatmalkawi29 <malkawifurat29@gmail.com>
2021-02-18 00:23:14 +0200

    ok4

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let score = 0;
 
 //Welcoming -------------------------------------------------------------------
 
-let inputName = prompt ('Enter Your Name Please:', 'Furat');
+let inputName = prompt ( 'Enter Your Name Please:', 'Furat' );
 
 alert ( 'Welcome ' + inputName + '!' );
 
@@ -14,42 +14,66 @@ alert ( 'Welcome ' + inputName + '!' );
 // Guessing Game:
 //Q1
 
-  let photography = prompt ('Do I Enjoy Photography? (y/n) or (yes/no)' , 'y' ).toLowerCase(); //get input
+//pair programming
+/*let photography = prompt ( 'Do I Enjoy Photography? (y/n) or (yes/no)' , 'y' ).toLowerCase(); //get input
 
 
-   const q1Function  = function (photographyvalue){
-   photography = photographyvalue.toLowerCase();
+const q1Function  = function ( photographyvalue )
+{
+  photography = photographyvalue.toLowerCase();
 
-    while (photography !== 'n' && photography !== 'no' && photography !=='yes' && photography !== 'y' ) 
-    { // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
-      photography = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
-    }
+  while ( photography !== 'n' && photography !== 'no' && photography !=='yes' && photography !== 'y' ) 
+  { 
+    // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+    photography = prompt ( 'Error: Enter (y/n) or (yes/no)' , 'y' ).toLowerCase(); // keep asking for input
+  }
 
 
-    if (photography==='y'|| photography==='yes') // if answer is yes or y ---> alert
-    {
-      //console.log('Correct Answer! Yes, I Enjoy Photography? ');
-      alert ('Correct Answer! Yes, I Enjoy Photography? ');
-      score = score + 1;
-    } 
+  if ( photography==='y'|| photography==='yes' ) // if answer is yes or y ---> alert
+  {
+    //console.log('Correct Answer! Yes, I Enjoy Photography? ');
+    alert ('Correct Answer! Yes, I Enjoy Photography? ');
+    score = score + 1;
+  } 
     
-    else if (photography==='n'|| photography==='no') // check if answer is no or n ----> alert
-    {
-      alert ('Wrong Answer! NO, I Enjoy Photography? ');
-    }
-    }
+  else if ( photography==='n'|| photography==='no' ) // check if answer is no or n ----> alert
+  {
+    alert ( 'Wrong Answer! NO, I Enjoy Photography?' );
+  }
+}
 
-    q1Function(photography);
+q1Function( photography );
+*/  
+
+let photography = prompt ( 'Do I Enjoy Photography? (y/n) or (yes/no)' , 'y' ).toLowerCase(); //get input
+
+while ( photography !== 'n' && photography !== 'no' && photography !=='yes' && photography !== 'y' ) 
+{ 
+  // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+  photography = prompt ( 'Error: Enter (y/n) or (yes/no)' , 'y' ).toLowerCase(); // keep asking for input
+}
+
+
+if ( photography==='y'|| photography==='yes' ) // if answer is yes or y ---> alert
+{
+  //console.log('Correct Answer! Yes, I Enjoy Photography? ');
+  alert ('Correct Answer! Yes, I Enjoy Photography? ');
+  score = score + 1;
+} 
   
-
+else if ( photography==='n'|| photography==='no' ) // check if answer is no or n ----> alert
+{
+  alert ( 'Wrong Answer! NO, I Enjoy Photography?' );
+}
 
 
 //-----------------------------------------------------------------------------
 //Q2
-let siblings = prompt ('Do I Have Older Siblings? (y/n) or (yes/no)' , 'n' ).toLowerCase();;
+let siblings = prompt ( 'Do I Have Older Siblings? (y/n) or (yes/no)' , 'n' ).toLowerCase();;
 
-while (siblings !== 'n' && siblings !== 'no' && siblings !=='yes' && siblings !== 'y' ) 
-{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+while ( siblings !== 'n' && siblings !== 'no' && siblings !=='yes' && siblings !== 'y' ) 
+{ 
+  // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
   siblings = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
 }
 
@@ -78,7 +102,8 @@ carDriving = carDriving.toLowerCase();
 
 
 while (carDriving !== 'n' && carDriving !== 'no' && carDriving !=='yes' && carDriving !== 'y' ) 
-{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+{ 
+  // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
   carDriving = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
 }
 
@@ -94,7 +119,7 @@ if (carDriving==='n'|| carDriving==='no') //check if answer no / n
 
 else if (carDriving==='y'|| carDriving==='yes') // check if answer is yes or y 
 {
-alert ('Wrong Answer! NO, I Can not Drive a Car Yet ');
+  alert ('Wrong Answer! NO, I Can not Drive a Car Yet ');
 }
 
 
@@ -107,7 +132,8 @@ let catPerson = prompt ('Am I a Cat Person? (y/n) or (yes/no)' , 'y' );
 catPerson = catPerson.toLowerCase();
 
 while (catPerson !== 'n' && catPerson !== 'no' && catPerson !=='yes' && catPerson !== 'y' ) 
-{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+{ 
+  // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
   catPerson = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
 }
 
@@ -136,7 +162,8 @@ let sweetTooth = prompt ('Am I a Sweet-tooth? (y/n) or (yes/no)' , 'y' );
 sweetTooth = sweetTooth.toLowerCase();
 
 while (sweetTooth !== 'n' && sweetTooth !== 'no' && sweetTooth !=='yes' && sweetTooth !== 'y' ) 
-{ // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
+{ 
+  // check input //run only in one case: all statements : false -----> input != y, yes, n, no 
   sweetTooth = prompt ('Error: Enter (y/n) or (yes/no)', 'y' ).toLowerCase(); // keep asking for input
 }
 
@@ -162,36 +189,33 @@ else if (sweetTooth==='n'|| sweetTooth==='no') // check if answer is no or n
 //-------------------------------------------------------------------------------
 
 //Q6 (Number)
-let numberInput = prompt ('Lets PLAY! | Guess What Number I Have In My Head ! ' , '5');
 
+let numberInput ;
 
-  if (numberInput == 5) // Equal 5 alert
-  {
-    alert ('CONGRAGULATIONS!! You Guessed! Its : 5');
-    score = score + 1;
-  }
-  
-  //-----------------
-
-  for (let i=0 ; i< 3 ;i++) // 4 attempts (4 prompts)
+for (let i=0 ; i< 4 ;i++) // 4 attempts (4 prompts)
 {
+  numberInput = prompt ('Lets PLAY! | Guess What Number I Have In My Head ! ' , '5');
+  numberInput = Number (numberInput);
+
   if ( numberInput > 5)  // Bigger than 5
   {
-    numberInput = prompt ('TRY AGAIN : Too High!' , '5');
-    
+    alert ('TRY AGAIN : Too High!'); 
   } else if ( numberInput < 5)  //less Than 5
   {
-    numberInput = prompt ('TRY AGAIN : Too Low!' , '5');
+    alert ('TRY AGAIN : Too Low!');
   }  
+  if (numberInput == 5 ) // Equal 5
+  {
+    alert ('You Guessed right! Its : 5');
+    score =score+1; 
+    break;
+  }
 }
 
-if (numberInput != 5 ) // Doesnt Equal 5 alert
+if (numberInput !== 5 ) // Doesnt Equal 5 alert
 {
   alert ('You didnt Guess! Its : 5'); 
-
 }
-
-
 
 
 //------------------------------------------------------------------------------------
@@ -201,42 +225,50 @@ let arrChocolate =['galaxy','snickers','twix'];
 
 let answer ;
 
-function successful () 
+
+answer = prompt ('Whats My favorite chocolate?' , 'Galaxy').toLowerCase (); //first attempt
+function successful() 
 {
-for (let i=0; i<3 ;i++) //move inside array ---> if answer exist ---> alert choices 
+  for (let i=0; i<3 ;i++) //move inside array ---> if answer exist ---> alert choices 
   {
-    if (answer == arrChocolate[i])
+    if (answer === arrChocolate[i])
     {
       alert ('CONGRAGULATIONS!! You Guessed! MY Favorites are : Galaxy, Mars, Twix '); 
-      score = score + 1;
-      return 1 ;
-      break;    // get outside for --> to show alert one time 
+      return 1 ; // get outside function --> to show alert one time 
     }
   }
 }
 
+let returnValue;
+returnValue = successful(); 
+
 //-----------------------------------------
 
-successful (); // first attempt (1 promt)
-
-let returnValue = 0;
-
-  if (!returnValue) // excute ---> if successful didnt return 1 (if answer still wrong)
+if (returnValue === 1)
 {
-  for (let i=0; i<6 ;i++) // (5 more prompt)
+  score = score + 1 ;
+}
+
+
+if (!returnValue) // excute ---> if successful didnt return 1 (if answer still wrong)
+{
+  for (let i=0; i<5 ;i++) // (5 more prompt)
   { 
-    if (answer != arrChocolate[0] && answer != arrChocolate[1] && answer != arrChocolate[2]) // excute is answer is wrong
+    if (answer !== arrChocolate[0] && answer !== arrChocolate[1] && answer !== arrChocolate[2]) // excute is answer is wrong
     {
-    answer = prompt ('Whats My favorite chocolate?' , 'Galaxy').toLowerCase (); 
-    returnValue = successful(); // excute function 
+      answer = prompt ('Whats My favorite chocolate?' , 'Galaxy').toLowerCase (); 
+      returnValue = successful(); // excute function
+      if (returnValue === 1)
+      {
+        score = score + 1;
+      }
     }
   } 
 }
 
-if (answer != arrChocolate[0] && answer != arrChocolate[1] && answer != arrChocolate[2] ) // if answer still wrong after attempts
+if (answer !== arrChocolate[0] && answer !== arrChocolate[1] && answer !== arrChocolate[2] ) // if answer still wrong after attempts
 {
-alert ('You Didnt Guess Right! MY Favorites are : Galaxy, Mars, Twix '); // ---> alert answer
- 
+  alert ('You Didnt Guess Right! MY Favorites are : Galaxy, Mars, Twix '); // ---> alert answer
 }
 
 
